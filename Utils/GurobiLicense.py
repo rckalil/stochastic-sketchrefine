1d7b9ae0d2dbcd7de7eb764897f0d04060d42c07
Rodrigo Cavalcante Kalil <rodrigockalil@gmail.com>
2025-09-23 15:12:52 -0300

    Made some weeks ago

--- Utils/GurobiLicense.py
+++ Utils/GurobiLicense.py
@@ -1,6 +1,6 @@
 class GurobiLicense:
     OPTIONS = {
-        "WLSACCESSID": "...",
-        "WLSSECRET": "...",
-        "LICENSEID": 000,
+        "WLSACCESSID": "3f8218cb-3d91-42a3-80d1-423d60e9916b",
+        "WLSSECRET": "dcd48d3a-bd4e-4b25-8669-0f4f3ccad5b0",
+        "LICENSEID": "2664792"
     }
