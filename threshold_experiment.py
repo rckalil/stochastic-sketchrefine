bde68b0d689ab04127566bb405d37d3c6a4c2a4a
Rodrigo Cavalcante Kalil <rodrigockalil@gmail.com>
2025-10-10 04:32:01 -0300

    Experiment run

--- threshold_experiment.py
+++ threshold_experiment.py
@@ -43,7 +43,7 @@ if __name__ == '__main__':
     SeedManager.reinitialize_seed()
     # print("Arroz")
 
-    for gain_threshold in range(2200, 5050, 500):
+    for gain_threshold in range(-100, 800, 100):
         print("Berinjela")
         print('Gain threshold:', gain_threshold)
         formatted_query = query_template[4] % str(gain_threshold)
@@ -53,7 +53,7 @@ if __name__ == '__main__':
         query = Parser().parse(query_lines)
         print('Parsed query:', query)
 
-        """start_time = time.time()
+        start_time = time.time()
         summarySearch = SummarySearch(
             query=query, linear_relaxation=False,
             dbInfo=PortfolioInfo, init_no_of_scenarios=100,
@@ -72,9 +72,9 @@ if __name__ == '__main__':
         # print('Total time for gain threshold', gain_threshold, 'is', end_time - start_time, 'secs')
         # print('===================================')
         with open("tr.txt", "a") as f:
-            f.write(f"{gain_threshold},{end_time - start_time},{resultado}\n")"""
+            f.write(f"{gain_threshold},{end_time - start_time},{resultado}\n")
 
-        start_time = time.time()
+        """start_time = time.time()
         rclsolve = RCLSolve(
             query=query, linear_relaxation=False,
             dbInfo=PortfolioInfo, init_no_of_scenarios=100,
@@ -91,4 +91,4 @@ if __name__ == '__main__':
         end_time = time.time()
         with open("tr_rcl.txt", "a") as f:
             f.write(f"{gain_threshold},{end_time - start_time},{package_dict}\n")
-        
+        """
