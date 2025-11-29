c8344574da40e770614786152beb1aff185e11e4
Rodrigo Cavalcante Kalil <rodrigockalil@gmail.com>
2025-11-29 11:58:54 -0300

    Update julia_ex.jl

--- julia_ex.jl
+++ julia_ex.jl
@@ -1,21 +1,45 @@
 using Random: MersenneTwister
 using SamplingRB
+using CSV
+using DataFrames
 
-rng = MersenneTwister(1)
+function get_actions(path)
+    println("Getting source")
+    data = CSV.read(path, DataFrame)
+    return data
+end
 
-# Parameters
-d    = 3  # dimension
-nsim = 10 # Nb of simulations
+function simulate(actions, n_sim)
+    println("Actions")
 
-B = ones(d)
-alpha = 0.90
-relative_losses = randn(rng, d, nsim)
+end
 
-status, w = cvar_rbp(B, alpha, relative_losses)
+function call_optimizer(relative_losses, alpha=0.9)
+    println("Optimize")
+end
 
-println("Finished")
-@assert status == 0
-println(w)
-@assert isapprox(w, [0.2280, 0.2706, 0.5014]; atol=1e-4)
 
-println(2)
+function run()
+    rng = MersenneTwister(1)
+
+    println(rng)
+
+    # Parameters
+    d    = 3  # dimension
+    nsim = 10 # Nb of simulations
+
+    B = ones(d)
+    alpha = 0.90
+    relative_losses = randn(rng, d, nsim)
+    println(length(relative_losses))
+
+    status, w = cvar_rbp(B, alpha, relative_losses)
+
+    println("Finished")
+    println(status)
+    # @assert status == 0
+    println(w)
+    # @assert isapprox(w, [0.2280, 0.2706, 0.5014]; atol=1e-4)
+
+    println(2)
+end
